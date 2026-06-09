98b74c0f78db29cd33fe8dcb9050c27e7040bf6c
Shailesh Shukla <shailesh.shukla29.in@gmail.com>
2026-06-09 17:01:00 +0530

    feat : Added a joystick in controller page

--- lg_rpg_controller/lib/ui/pages/controller_page.dart
+++ lg_rpg_controller/lib/ui/pages/controller_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_joystick/flutter_joystick.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lg_rpg_controller/ui/providers/game_providers.dart';
 
 class ControllerPage extends ConsumerStatefulWidget {
   const ControllerPage({super.key});
@@ -10,12 +13,41 @@ class ControllerPage extends ConsumerStatefulWidget {
 
 class _ControllerPageState extends ConsumerState<ControllerPage> {
   @override
+  void initState() {
+    super.initState();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'ControllerPage',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    return Scaffold(
+      body: SafeArea(
+        child: Row(
+          children: [
+            Expanded(
+              child: Center(
+                child: Joystick(
+                  listener: (details) {
+                    ref
+                        .read(movePlayerUseCaseProvider)
+                        .call(details.x, details.y);
+                  },
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
