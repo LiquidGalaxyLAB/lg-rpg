5b8d087773f0b1d4f8773f9db4c817571c053dd6
Shailesh Shukla <shailesh.shukla29.in@gmail.com>
2026-06-11 16:57:51 +0530

    feat(game-server): add game started event stream to repository API

--- lg_rpg_controller/lib/domain/repositories/game_server_repository.dart
+++ lg_rpg_controller/lib/domain/repositories/game_server_repository.dart
@@ -1,10 +1,13 @@
 import 'package:lg_rpg_controller/domain/entities/lobby_entity.dart';
 import 'package:lg_rpg_controller/domain/entities/game_server_entity.dart';
+import 'package:lg_rpg_controller/domain/entities/game_started_entity.dart';
 
 abstract class GameServerRepository {
   Stream<GameServerEntity> get serverStatusStream;
 
   Stream<LobbyEntity?> get lobbyStream;
+
+  Stream<GameStartedEntity> get gameStartedStream;
 
   bool get isGameConnected;
 
