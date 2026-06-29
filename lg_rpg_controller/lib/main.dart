4dbf724880f779ee792923cae25411f1198bfbd4
Shailesh Shukla <shailesh.shukla29.in@gmail.com>
2026-06-29 18:05:08 +0530

    fix(controller): correct theme import path and add missing respawn provider

--- lg_rpg_controller/lib/main.dart
+++ lg_rpg_controller/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lg_rpg_controller/ui/pages/main_screen.dart';
-import 'package:lg_rpg_controller/ui/theme/app_theme.dart';
+import 'package:lg_rpg_controller/core/theme/app_theme.dart';
 
 void main() async {
   runApp(
